a26e05ff1953daaa5e5b5e8831a9d86f21fd89c9
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-03-06 18:19:29 -0800

    setup dev plugin

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    `java-gradle-plugin`
+    alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
+}
+
+group = "com.zenith"
+version = "1.0.0-SNAPSHOT"
+val pluginId = "zenithproxy-dev"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    api(libs.idea.ext)
+}
+
+java {
+    withSourcesJar()
+}
+
+gradlePlugin {
+    // Define the plugin
+    val zenithPlugin by plugins.creating {
+        id = pluginId
+        implementationClass = "com.zenith.ZenithProxyDevGradlePlugin"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "vc"
+            url = uri("https://maven.2b2t.vc/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+}
